a0413d7deaac61d4d379017ad42fcaf04e3339cd
Hai <hainguyen.8y@gmail.com>
2019-09-06 08:41:15 +0700

    Fixed delete snapshot with dependency (resumable)

--- destroy.go
+++ destroy.go
@@ -1,20 +1,14 @@
 package zfs
 
+// #include <stdio.h>
 // #include <stdlib.h>
 // #include <libzfs.h>
 // #include "common.h"
 // #include "zpool.h"
 // #include "zfs.h"
-// int snapshot_to_nvl_cb(zfs_handle_t *zhp, void *arg)
-// {
-// 	int err = 0;
-// 	nvlist_t *pnvl = (nvlist_t*) arg;
-//
-// 	if (nvlist_add_boolean(pnvl, zfs_get_name(zhp))) {
-// 		return -1;
-// 	}
-// 	return (err);
-// }
+// void __printf(char *key, char *val);
+// int snapshot_to_nvl_cb(zfs_handle_t *zhp, void *arg);
+// void print_list(nvlist_t *pnvl);
 import "C"
 
 import (
@@ -24,15 +18,18 @@ import (
 	"unsafe"
 )
 
+//export __printf
+func __printf(k, v *C.char) {
+	fmt.Printf("%s=%s\n", C.GoString(k), C.GoString(v))
+}
+
 func DestroySnapshot(pathname string) (err error) {
 	at := strings.Index(pathname, "@")
 	if at == -1 {
 		return errors.New("not snapshot")
 	}
 	dtpath := C.CString(pathname[:at])
-
 	snapspec := C.CString(pathname[at+1:])
-
 	nvl := C.fnvlist_alloc();
 	if nvl == nil {
 		return errors.New("not allocated memory")
@@ -45,9 +42,11 @@ func DestroySnapshot(pathname string) (err error) {
 	}
 	defer C.zfs_close(zhp)
 
-	cerr := C.zfs_iter_snapspec(zhp, snapspec, (C.zfs_iter_f)(unsafe.Pointer(C.snapshot_to_nvl_cb)), unsafe.Pointer(nvl));
-	if cerr != C.ENOENT {
-		return fmt.Errorf("iter snapspec: %s", C.GoString(C.libzfs_last_error_str()))
+	zhpdup := C.zfs_handle_dup(zhp)
+	defer C.zfs_close(zhpdup)
+	cerr := C.zfs_iter_snapspec(zhpdup, snapspec, (C.zfs_iter_f)(unsafe.Pointer(C.snapshot_to_nvl_cb)), unsafe.Pointer(nvl));
+	if cerr != 0 && cerr != C.ENOENT {
+		return fmt.Errorf("iter snapspec %d: %s", int(cerr), C.GoString(C.libzfs_last_error_str()))
 	}
 
 	if C.nvlist_empty(nvl) == C.B_TRUE {
