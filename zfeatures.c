38a6ae03114fb7e022eaed8facd406837f7dd772
Hai <hainguyen.8y@gmail.com>
2020-02-05 16:46:18 +0700

    Fix compability between zfs-0.7 and zfs-0.8

--- zfeatures.c
+++ zfeatures.c
@@ -8,6 +8,9 @@
 #include "zpool.h"
 #include "zfs.h"
 
+/*
+// WARN: the structure is different betwen 0.7 and 0.8
 const char *zfeatures_get_name(int index) {
     return spa_feature_table[index].fi_uname;
 }
+*/
--- zfeatures.h
+++ zfeatures.h
@@ -2,8 +2,9 @@
 #ifndef __ZFEATURES_H__
 #define __ZFEATURES_H__
 
-#include <zfeature_common.h>
+//#include <zfeature_common.h>
 
-const char *zfeatures_get_name(int index);
+// binding to the specific libzfs version used
+//const char *zfeatures_get_name(int index);
 
 #endif //__ZFEATURES_H__
